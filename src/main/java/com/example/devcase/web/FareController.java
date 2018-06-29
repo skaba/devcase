221f837c8f6fa960bb7e63416a29b2b88db8d25a
Serkan Kaba <serkan_kaba@yahoo.com>
2018-06-29 09:32:35 +0200

    Remove unused

--- src/main/java/com/example/devcase/web/FareController.java
+++ src/main/java/com/example/devcase/web/FareController.java
@@ -1,7 +1,5 @@
 package com.example.devcase.web;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
