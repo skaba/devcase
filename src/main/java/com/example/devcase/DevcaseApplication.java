290a44b781942a536e8bbb2ea9186fd8afb74cef
Serkan Kaba <serkan_kaba@yahoo.com>
2018-06-29 07:58:13 +0200

    Oauth2 client support.

--- src/main/java/com/example/devcase/DevcaseApplication.java
+++ src/main/java/com/example/devcase/DevcaseApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableOAuth2Client
 public class DevcaseApplication {
 
 	public static void main(String[] args) {
@@ -15,7 +19,8 @@ public class DevcaseApplication {
 	}
 
 	@Bean
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
+	public OAuth2RestTemplate oAuth2RestTemplate(final ClientCredentialsResourceDetails resource,
+			final OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
 	}
 }
